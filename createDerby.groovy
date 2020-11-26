e7f9827ff6b99c70d98488a1d434b701fdec8dc9
DeniseSl22 <denise.slenter@maastrichtuniversity.nl>
2020-11-26 10:30:38 +0100

    Starting create file with EC, Kegg-R and metaCyc

--- createDerby.groovy
+++ createDerby.groovy
@@ -31,6 +31,7 @@ blacklist = new HashSet<String>();
 //blacklist.add("C00350") //Example of blacklist item.
 
 ////Registering Datasources to create mappings
+rheaDS = DataSource.register ("Rh", "Rhea").asDataSource()
 enzymenom = DataSource.register ("E", "Enzyme Nomenclature").asDataSource()
 keggreact = DataSource.register ("Rk", "KEGG Reaction").asDataSource()
 reactomereact = DataSource.register ("Re", "Reactome").asDataSource()
@@ -38,11 +39,6 @@ metacycDS = DataSource.register ("Mc", "MetaCyc").asDataSource() //Not recognise
 ecocycDS = DataSource.register ("Eco", "EcoCyc").asDataSource() //Not recognised as BioDataSource
 macieDS = DataSource.register ("Ma", "MACiE").asDataSource() //Not recognised as BioDataSource
 unipathDS = DataSource.register ("Up", "Unipathway").asDataSource() //Not recognised as BioDataSource
-//Old, can be removed after test run:
-wikidataDS = DataSource.register ("Wd", "Wikidata").asDataSource()
-omimDS = BioDataSource.OMIM //SysCode: Om
-doDS = DataSource.register ("Do", "Disease Ontology").asDataSource() //Syscode part of BridgeDb library (not released officially yet).
-
 
 String dateStr = new SimpleDateFormat("yyyyMMdd").format(new Date());
 database.setInfo("BUILDDATE", dateStr);
@@ -88,227 +84,101 @@ def addAttribute(GdbConstruct database, Xref ref, String key, String value) {
 
 //// load the Rhea content
 
-/*
-
-// Rhea-EC
+// Split up data per column
 counter = 0
 error = 0
-new File("omim2wikidata.csv").eachLine { line,number ->
+new File("data/mappingsrhea.tsv").eachLine { line,number ->
   if (number == 1) return // skip the first line
 
-  fields = line.split(",")
-  rootid = fields[0].substring(31)
-  Xref ref = new Xref(rootid, wikidataDS);
-  if (!genesDone.contains(ref.toString())) {
-    addError = database.addGene(ref);
-    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
-    error += addError
-    linkError = database.addLink(ref,ref);
-    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
-    error += linkError
-    genesDone.add(ref.toString())
+
+	fields = line.split(/\t/)
+	def rootid ;
+	def direction ;
+	def masterid ;
+	def databaseid ;
+	def databasename ;
+	if (fields.size() >= 5) {
+		(rootid, direction, masterid, databaseid, databasename) = line.split(/\t/) 
+	}
+        //if (number == 3) println databasename ////Test if data is read correctly
+	//if (number == 4) println rootid	////Test if data is read correctly
+
+  
+
+ //Rhea-EC 
+	if(databasename == "EC" ) {  //Only use the 4th column for EC mappings if it contains "EC".
+	  Xref ref = new Xref(rootid, rheaDS);
+	  if (!genesDone.contains(ref.toString())) {
+	    addError = database.addGene(ref);
+	    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
+	      error += addError
+	      linkError = database.addLink(ref,ref);
+	    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
+	      error += linkError
+	      genesDone.add(ref.toString())
+    }
+
+	  // add external identifiers
+	  addXRef(database, ref, databaseid, enzymenom, genesDone, linksDone);	
+
+	  counter++
+	  if (counter % commitInterval == 0) {
+      	    println "Info: errors: " + error + " (EC Nomenclature)"
+     	    database.commit()
+	}
   }
 
-  // add external identifiers
-  addXRef(database, ref, fields[1], omimDS, genesDone, linksDone);
+ //Rhea-KEGG 
+	if(databasename == "KEGG_REACTION" ) {  //Only use the 4th column for EC mappings if it contains "EC".
+	  Xref ref = new Xref(rootid, rheaDS);
+	  if (!genesDone.contains(ref.toString())) {
+	    addError = database.addGene(ref);
+	    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
+	      error += addError
+	      linkError = database.addLink(ref,ref);
+	    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
+	      error += linkError
+	      genesDone.add(ref.toString())
+    }
 
-  counter++
-  if (counter % commitInterval == 0) {
-    println "Info: errors: " + error + " (OMIM)"
-    database.commit()
+	  // add external identifiers
+	  addXRef(database, ref, databaseid, keggreact, genesDone, linksDone);	
+
+	  counter++
+	  if (counter % commitInterval == 0) {
+      	    println "Info: errors: " + error + " (KEGG Reactions)"
+     	    database.commit()
+	}
   }
+
+ //Rhea-MetaCyC
+	if(databasename == "METACYC" ) {  //Only use the 4th column for EC mappings if it contains "EC".
+	  Xref ref = new Xref(rootid, rheaDS);
+	  if (!genesDone.contains(ref.toString())) {
+	    addError = database.addGene(ref);
+	    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
+	      error += addError
+	      linkError = database.addLink(ref,ref);
+	    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
+	      error += linkError
+	      genesDone.add(ref.toString())
+    }
+
+	  // add external identifiers
+	  addXRef(database, ref, databaseid, metacycDS, genesDone, linksDone);	
+
+	  counter++
+	  if (counter % commitInterval == 0) {
+      	    println "Info: errors: " + error + " (MetaCyc)"
+     	    database.commit()
+	}
+  }
+
+
 }
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" //Not implemented (yet) for disease IDs.
-
-
-// Rhea-Kegg
-counter = 0
-error = 0
-new File("do2wikidata.csv").eachLine { line,number ->
-  if (number == 1) return // skip the first line
-
-  fields = line.split(",")
-  rootid = fields[0].substring(31)
-  Xref ref = new Xref(rootid, wikidataDS);
-  if (!genesDone.contains(ref.toString())) {
-    addError = database.addGene(ref);
-    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
-    error += addError
-    linkError = database.addLink(ref,ref);
-    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
-    error += linkError
-    genesDone.add(ref.toString())
-  }
-
-  // add external identifiers
-  addXRef(database, ref, fields[1], doDS, genesDone, linksDone);
-
-  counter++
-  if (counter % commitInterval == 0) {
-    println "Info: errors: " + error + " (DiseaseOntology)"
-    database.commit()
-  }
-}
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" //Not implemented (yet) for disease IDs.
-
-
-// Rhea-Reactome
-counter = 0
-error = 0
-new File("cui2wikidata.csv").eachLine { line,number ->
-  if (number == 1) return // skip the first line
-
-  fields = line.split(",")
-  rootid = fields[0].substring(31)
-  Xref ref = new Xref(rootid, wikidataDS);
-  if (!genesDone.contains(ref.toString())) {
-    addError = database.addGene(ref);
-    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
-    error += addError
-    linkError = database.addLink(ref,ref);
-    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
-    error += linkError
-    genesDone.add(ref.toString())
-  }
-
-  // add external identifiers
-  addXRef(database, ref, fields[1], cuiDS, genesDone, linksDone);
-
-  counter++
-  if (counter % commitInterval == 0) {
-    println "Info: errors: " + error + " (UMLSCUI)"
-    database.commit()
-  }
-}
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" //Not implemented (yet) for disease IDs.
-
-
-
-// Rhea-Metacyc
-counter = 0
-error = 0
-new File("orpha2wikidata.csv").eachLine { line,number ->
-  if (number == 1) return // skip the first line
-
-  fields = line.split(",")
-  rootid = fields[0].substring(31)
-  Xref ref = new Xref(rootid, wikidataDS);
-  if (!genesDone.contains(ref.toString())) {
-    addError = database.addGene(ref);
-    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
-    error += addError
-    linkError = database.addLink(ref,ref);
-    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
-    error += linkError
-    genesDone.add(ref.toString())
-  }
-
-  // add external identifiers
-  addXRef(database, ref, fields[1], orphaDS, genesDone, linksDone);
-
-  counter++
-  if (counter % commitInterval == 0) {
-    println "Info: errors: " + error + " (OrphaNet)"
-    database.commit()
-  }
-}
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" //Not implemented (yet) for disease IDs.
-
-
-// Rhea Ecocyc
-counter = 0
-error = 0
-new File("mesh2wikidata.csv").eachLine { line,number ->
-  if (number == 1) return // skip the first line
-
-  fields = line.split(",")
-  rootid = fields[0].substring(31)
-  Xref ref = new Xref(rootid, wikidataDS);
-  if (!genesDone.contains(ref.toString())) {
-    addError = database.addGene(ref);
-    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
-    error += addError
-    linkError = database.addLink(ref,ref);
-    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
-    error += linkError
-    genesDone.add(ref.toString())
-  }
-
-  // add external identifiers
-  addXRef(database, ref, fields[1], meshDS, genesDone, linksDone);
-
-  counter++
-  if (counter % commitInterval == 0) {
-    println "Info: errors: " + error + " (MeSH)"
-    database.commit()
-  }
-}
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" //Not implemented (yet) for disease IDs.
-
-// rhea - macie 
-counter = 0
-error = 0
-new File("icd92wikidata.csv").eachLine { line,number ->
-  if (number == 1) return // skip the first line
-
-  fields = line.split(",")
-  rootid = fields[0].substring(31)
-  Xref ref = new Xref(rootid, wikidataDS);
-  if (!genesDone.contains(ref.toString())) {
-    addError = database.addGene(ref);
-    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
-    error += addError
-    linkError = database.addLink(ref,ref);
-    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
-    error += linkError
-    genesDone.add(ref.toString())
-  }
-
-  // add external identifiers
-  addXRef(database, ref, fields[1], icd9DS, genesDone, linksDone);
-
-  counter++
-  if (counter % commitInterval == 0) {
-    println "Info: errors: " + error + " (ICD-9)"
-    database.commit()
-  }
-}
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" //Not implemented (yet) for disease IDs.
-
-
-// rhea-unipathway
-counter = 0
-error = 0
-new File("icd102wikidata.csv").eachLine { line,number ->
-  if (number == 1) return // skip the first line
-
-  fields = line.split(",")
-  rootid = fields[0].substring(31)
-  Xref ref = new Xref(rootid, wikidataDS);
-  if (!genesDone.contains(ref.toString())) {
-    addError = database.addGene(ref);
-    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
-    error += addError
-    linkError = database.addLink(ref,ref);
-    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
-    error += linkError
-    genesDone.add(ref.toString())
-  }
-
-  // add external identifiers
-  addXRef(database, ref, fields[1], icd10DS, genesDone, linksDone);
-
-  counter++
-  if (counter % commitInterval == 0) {
-    println "Info: errors: " + error + " (ICD-10)"
-    database.commit()
-  }
-}
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" //Not implemented (yet) for disease IDs.
-//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"NamesFound\"/>\n"
+//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"CASNumbersFound\"/>\n" 	//No tests implemented (yet) for interaction IDs.
+//unitReport << "  <testcase classname=\"WikidataCreation\" name=\"NamesFound\"/>\n"		///These lines are merely here as examples
 //unitReport << "</testsuite>\n"
-
-*/
 
 database.commit();
 database.finalize();
